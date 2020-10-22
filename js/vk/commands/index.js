c19be7433271a36bb235cb2e0a7f1a864782a129
Denis <denisstep99@gmail.com>
2020-10-22 22:41:08 +0300

    commands export

---
+++ js/vk/commands/index.js
@@ -0,0 +1,21 @@
+const today = require('./today');
+const tomorrow = require('./tomorrow');
+const week = require('./week');
+const weekday = require('./weekday');
+const showAllCommands = require('./show-all-commands');
+const afterTomorrow = require('./after-tomorrow');
+const start = require('./start');
+const subscribe = require('./subscribe')
+const byDate = require('./by-date')
+
+module.exports = {
+  today,
+  tomorrow,
+  week,
+  weekday,
+  showAllCommands,
+  afterTomorrow,
+  start,
+  subscribe,
+  byDate
+}
--- js/vk/index.js
+++ js/vk/index.js
@@ -1,28 +1,20 @@
 const VkBot = require('node-vk-bot-api');
 const {VK_TOKEN} = require("../constants");
-
-const today = require('./commands/today');
-const tomorrow = require('./commands/tomorrow');
-const week = require('./commands/week');
-const weekday = require('./commands/weekday');
+const commands = require('./commands');
 const {onStart, onUpdate} = require("./event-handlers");
-const showAllCommands = require('./commands/show-all-commands');
-const afterTomorrow = require('./commands/after-tomorrow');
-const start = require('./commands/start');
-const subsctibe = require('./commands/subscribe')
-const byDate = require('./commands/by-date')
+
 
 const bot = new VkBot({token: VK_TOKEN});
 
-bot.command(today.keySet, today.command);
-bot.command(tomorrow.keySet, tomorrow.command);
-bot.command(week.keySet, week.command);
-bot.command(weekday.keySet, weekday.command);
-bot.command(showAllCommands.keySet, showAllCommands.command);
-bot.command(afterTomorrow.keySet, afterTomorrow.command);
-bot.command(start.keySet, start.command);
-bot.command(byDate.keySet, byDate.command);
-bot.command(subsctibe.keySet, subsctibe.command)
+bot.command(commands.today.keySet, commands.today.command);
+bot.command(commands.tomorrow.keySet, commands.tomorrow.command);
+bot.command(commands.week.keySet, commands.week.command);
+bot.command(commands.weekday.keySet, commands.weekday.command);
+bot.command(commands.showAllCommands.keySet, commands.showAllCommands.command);
+bot.command(commands.afterTomorrow.keySet, commands.afterTomorrow.command);
+bot.command(commands.start.keySet, commands.start.command);
+bot.command(commands.byDate.keySet, commands.byDate.command);
+bot.command(commands.subscribe.keySet, commands.subscribe.command)
 
 bot.startPolling((err) => {
     if (err) {
